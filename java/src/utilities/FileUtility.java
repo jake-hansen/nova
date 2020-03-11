271d26e0e8d4e955d2b7af9da757a88916646633
Jake Hansen <jakedhansen@outlook.com>
2020-03-11 18:44:01 -0500

    Add database connection utilities - Add DBPropertiesUtility - Add FileUtility - Add CheckAuthentication - Add DBConnection - Add config.properties.example

---
+++ java/src/utilities/FileUtility.java
@@ -0,0 +1,51 @@
+package utilities;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import javax.servlet.ServletContext;
+
+public class FileUtility {
+	public static List<String> readFile(ServletContext context, String filename) {
+		List<String> contents = new ArrayList<String>();
+		try {
+			InputStream is = context.getResourceAsStream(filename);
+			if (is != null) {
+				InputStreamReader isr = new InputStreamReader(is);
+				BufferedReader reader = new BufferedReader(isr);
+				String text;
+				while ((text = reader.readLine()) != null) {
+					contents.add(text);
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return contents;
+	}
+
+	public static List<String> readFile(String filePath) {
+		List<String> contents = new ArrayList<String>();
+		File file = new File(filePath);
+		Scanner scanner = null;
+		try {
+			scanner = new Scanner(file);
+			while (scanner.hasNextLine()) {
+				String line = scanner.nextLine();
+				contents.add(line);
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} finally {
+			if (scanner != null)
+				scanner.close();
+		}
+		return contents;
+	}
+}

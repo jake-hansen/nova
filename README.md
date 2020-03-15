b6d71890ed1e51da40768fe02775b57978abad7c
Jake Hansen <jakedhansen@outlook.com>
2020-03-15 12:00:40 -0500

    Updated authentication logic (#11)
    * Change project configuration to Maven
    - Rebuild project using Maven
    - Restructured directories
    - Moved www folder into webapp folder
    - Reformatted .gitignore
    
    * Change config file location
    - Change POST URL for local development
    
    * Restructure project
    - Separate authentication function
    - Remove Test Servlet from web.xml
    - Update pom.xml for new dependencies

--- README.md
+++ README.md
@@ -37,40 +37,15 @@ And you're done. Now you just need to setup your local Tomcat server.
 7. Search for *Server*
 8. Under server type, select *Tomcat v8.5 Server*
 9. Browse for the directory where Tomcat v8.5 is located
-10. Click *Finished*</br></br>
-And now you've configured your Tomcat server. All that is left is to build the
-project using Maven, and making sure it runs correctly on Tomcat!
-
-
-11. Right click on the project directory, choose *Maven* -> *Update Project* -> *OK*
-12. Now you can build the project. Right click on the project directory,
-choose *Run As* -> *Maven build...*
-13. In the 'Goals' field type **clean install** then click *Run*</br></br>
-You should see some lines scroll through the console at this point. This is
-the project being auto built and pulling in all necessary dependencies for you.
+10. Click *Finished*
+11. Right click on the project directory, choose *Maven* -> *Update Project* -> *OK*</br></br>
 
 At this point, you should be able to run the project on your local Tomcat
 server you configured earlier. In the *Run As* menu, choose **Run on Server**.
 Then choose *Finish*.
 
-
-<h5>Files you need to change</h5>
+<h4>Files you need to change</h4>
 It's important that after importing the project, you change some specific files. The requirements are below:
 
-1. *java/src/main/config.properites.example* - You'll need to copy this file to the same directory and rename it to **config.properties.private**. Then, change the required lines in order to connect to the database.
+1. *novaweb/src/main/resources/config/config.properites.example* - You'll need to copy this file to the same directory and rename it to **config.properties.private**. Then, change the required lines in order to connect to the database.
 
-2. *java/WebContent/WEB-INF/lib* - You'll need to add the **mysql-connector-java-8.0.12.jar file**. Make sure to add this jar to your build path.
-
-<h4>How to use Docker</h4>
-Docker can be used to setup a local HTTP server seamlessly without any manual install of a traditional HTTP server. To begin, you'll need to download the latest version of Docker for your platform.
-
-[Docker Downlaod](https://www.docker.com/get-started)
-
-Next, you'll need to either copy the provided docker/*startnovanginx.sh.example* file to *docker/startnovanginx.sh.private*
-and edit the copied file to include the source path to your local repo where necessary. Or, you can run the following commands to start the docker container:
-
-`docker build -t "nova-dev:1.0" . && \
-docker run -it --rm -d --name "nova" -p 80:80 -v \
-"LOCAL/PATH/TO/WWW/REPO/DIRECTORY":/usr/share/nginx/html:ro nova-dev:1.0`
-
-If you've done everything correctly, you should be able to navigate to http://localhost and be greeted with the website.
--- novaweb/src/main/java/webinterface/CheckAuthentication.java
+++ novaweb/src/main/java/webinterface/CheckAuthentication.java
@@ -1,18 +1,16 @@
 package webinterface;
 
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import databasetools.DBConnection;
+import authentication.CheckUserAuthentication;
 
 /**
  * Servlet implementation class CheckAuthentication
@@ -33,7 +31,7 @@ public class CheckAuthentication extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+
 		// Never respond to get requests, throw 404 error
 		// Need to implement this
 		response.getWriter().append("Served at: ").append(request.getContextPath());
@@ -43,57 +41,16 @@ public class CheckAuthentication extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		/**
-		 * Boolean to keep track of user authentication status.
-		 * Set to TRUE if user is authenticated. FALSE, otherwise.
-		 */
-		boolean isAuthenticated = false;
-		
-		// Make database connection
-		Connection connection = null;
-		DBConnection.connect();
-		connection = DBConnection.connection;
-		
-		// Get username from form
+
+		// Get username and password from POST
 		String username = request.getParameter("username");
-		
-		/**
-		 * Get password from form. NOTE: Storing password as a plaintext string
-		 * is insecure and should only be used during testing!
-		 */
 		String password = request.getParameter("password");
+
+		// Check if given username and password are valid
+		CheckUserAuthentication.check(username, password);
 		
-		// Check if username exists in database
-		PreparedStatement preparedStatement = null;
-		
-		// If username or password is empty, do not search database. User is not authenticated.
-		if (username.isEmpty() || password.isEmpty()) isAuthenticated = false;
-		
-		// Proceed with searching database.
-		else {
-			try {
-				preparedStatement = connection.prepareStatement("SELECT * FROM users WHERE email LIKE BINARY ? AND password LIKE BINARY ?");
-				preparedStatement.setString(1, username);
-				preparedStatement.setString(2, password);
-				ResultSet results = preparedStatement.executeQuery();
-				
-				// If a username and password match was found
-				if (results.next()) {
-					System.out.println("User found: " + username);
-				}
-				
-				// Username and password match was not found, user is not authenticated
-				else {
-					System.out.println("User not found.");
-				}
-			}
-			catch (SQLException se) {
-				se.printStackTrace();
-			}
-			catch (Exception e) {
-				e.printStackTrace();
-			}	
-		}	
+		// Get session
+		HttpSession session = request.getSession();
+		session.setAttribute("username", username);
 	}
 }

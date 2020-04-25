0304158f52eb69af874ca0d8720f306c2602c831
Jake Hansen <jacobdhansen@unomaha.edu>
2020-04-25 13:41:08 -0500

    Add empty String checking and commit to database
    Error checking for empty string implemented.
    
    Add logic to update existing userStatus record in database if it is
    found, otherwise create new userStatus record.

--- novaweb/src/main/java/setdata/AccountForMember.java
+++ novaweb/src/main/java/setdata/AccountForMember.java
@@ -1,6 +1,7 @@
 package setdata;
 
 import dao.UserDao;
+import dao.UserStatusDao;
 import datamodel.User;
 import datamodel.UserStatus;
 import utilities.ServletUtil;
@@ -11,6 +12,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
 /**
  * This Servlet is called when a user accounts for another member.
@@ -43,36 +45,60 @@ public class AccountForMember extends HttpServlet {
 
         // Get variables from POST
         String accountFor = request.getParameter("accountForName");
+        String firstName = "", lastName = "";
 
         //Parse out first and last name entered
-        String[] firstAndLast = accountFor.split("\\s+");
-        String firstName = firstAndLast[0];
-        String lastName = firstAndLast[1];
+        if (accountFor.contains(" ")) {
+            String[] firstAndLast = accountFor.split("\\s+");
+            firstName = firstAndLast[0];
+            lastName = firstAndLast[1];
+        }
 
-        // Create new UserDao connection
-        UserDao ud = new UserDao();
-
-        //Check if the user returns null then inform the user that this name isn't in our database
-        User user = ud.getByFirstAndLastName(firstName, lastName);
-        if (user == null) {
+        // Check if nothing was entered for first and last name
+        if (firstName.isEmpty() || lastName.isEmpty()) {
             request.getSession().setAttribute("failed_lookup", true);
+        } else {
+            // Create new UserDao connection
+            UserDao ud = new UserDao();
+
+            //Check if the user returns null then inform the user that this name isn't in our database
+            User user = ud.getByFirstAndLastName(firstName, lastName);
+
+            if (user == null) {
+                request.getSession().setAttribute("failed_lookup", true);
+            }
+
+            //User is in the database so set the user status
+            else {
+                // Get userID by user first and last name
+                int userID = user.getId();
+
+                //Create and set the looked up user status object
+                UserStatus userStatus = new UserStatus();
+                userStatus.setUserId(userID);
+
+                // Accounted for status code is 3; user is now accounted for
+                userStatus.setStatusCode(3);
+
+                // Attempt to update record in database, create record if it doesn't exist
+                UserStatusDao usd = new UserStatusDao();
+
+                List<UserStatus> usl = usd.getByField(UserStatus.class, "userId", Integer.toString(userStatus.getUserId()));
+
+                // Update existing record if found, otherwise create new record
+                if (!usl.isEmpty()) {
+                    userStatus.setId(usl.get(0).getId());
+                    usd.update(UserStatus.class, userStatus);
+                } else {
+                    usd.create(userStatus);
+                }
+
+                // Set boolean on fail check:
+                request.getSession().setAttribute("failed_lookup", false);
+            }
         }
 
-        //User is in the database so set the user status
-        else {
-            // Get userID by user first and last name
-            int userID = user.getId();
 
-            //Create and set the looked up user status object
-            UserStatus userStatus = new UserStatus();
-            userStatus.setUserId(userID);
-
-            // Accounted for status code is 3; user is now accounted for
-            userStatus.setStatusCode(3);
-
-            // Set boolean on fail check:
-            request.getSession().setAttribute("failed_lookup", false);
-        }
 
         // Forward to previous page
         ServletUtil.redirectToRequester(request, response);

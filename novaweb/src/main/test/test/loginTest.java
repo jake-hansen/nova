3385d66e06dccc34df40890b7e3f8843b4e7d7e4
Jake Hansen <jacobdhansen@unomaha.edu>
2020-04-26 16:09:47 -0500

    Add login test case

--- novaweb/src/main/test/test/loginTest.java
+++ novaweb/src/main/test/test/loginTest.java
@@ -2,14 +2,17 @@ package test;
 
 import java.util.regex.Pattern;
 import java.util.concurrent.TimeUnit;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.*;
 import static org.junit.Assert.*;
 import static org.hamcrest.CoreMatchers.*;
 import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.Select;
 
-public class test {
+public class loginTest {
     private WebDriver driver;
     private String baseUrl;
     private boolean acceptNextAlert = true;
@@ -17,19 +20,26 @@ public class test {
 
     @Before
     public void setUp() throws Exception {
-        System.setProperty("webdriver.gecko.driver", "test");
-        driver = new FirefoxDriver();
+        WebDriverManager.getInstance(ChromeDriver.class).setup();
+        driver = new ChromeDriver();
         baseUrl = "https://www.google.com/";
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
     }
 
     @Test
-    public void testUnoSearch() throws Exception {
-        driver.get("https://www.unomaha.edu/");
-        driver.findElement(By.id("gsc-i-id1")).click();
-        driver.findElement(By.id("gsc-i-id1")).clear();
-        driver.findElement(By.id("gsc-i-id1")).sendKeys("computer science");
-        driver.findElement(By.id("gsc-i-id1")).sendKeys(Keys.ENTER);
+    public void testLogin() throws Exception {
+        driver.get("http://localhost:8080/novaweb/");
+        driver.findElement(By.xpath("(//button[@type='button'])[2]")).click();
+        driver.findElement(By.id("email")).click();
+        driver.findElement(By.id("email")).clear();
+        driver.findElement(By.id("email")).sendKeys("testuser@testuser.com");
+        driver.findElement(By.id("password")).click();
+        driver.findElement(By.id("password")).clear();
+        driver.findElement(By.id("password")).sendKeys("password");
+        driver.findElement(By.xpath("//button[@type='submit']")).click();
+        assertTrue(isElementPresent(By.xpath("//button[@type='submit']")));
+        Thread.sleep(1000);
+        assertEquals("Logout", driver.findElement(By.xpath("//button[@type='submit']")).getText());
     }
 
     @After
